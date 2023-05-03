a0d1ee6284edd653f4d05bbc54363b28f03a8452
Gabriel Bussières <84876579+Simspace1@users.noreply.github.com>
2023-05-02 20:51:24 -0400

    Created the Start method for LoadRegions

--- Scripts/World/LoadRegions.cs
+++ Scripts/World/LoadRegions.cs
@@ -7,12 +7,23 @@ public class LoadRegions : MonoBehaviour
     [SerializeField]
     private World world;
 
-    List<RegionPos> region = new List<RegionPos>();
+    public static int maxRegionRadius = 10;
+    public static int fullResRegionRadius = 2;
+
+    List<RegionPos> regionList = new List<RegionPos>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int radius = 0; radius <= maxRegionRadius; radius++){
+            for(int x = 0; x <= radius; x++){
+                for(int z = 0; z <=radius; z++){
+                    if((Mathf.Abs(x) == radius || Mathf.Abs(z) == radius) && MyMath.Hypothenuse(x,z) <= maxRegionRadius){
+                        regionList.Add(new RegionPos(x,0,z));
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
