33a8fb1042e97d542ae48ac17bf63e6580055997
Gabriel Bussières <84876579+Simspace1@users.noreply.github.com>
2023-05-02 20:31:25 -0400

    Renamed LoadChunks2 to LoadRegions

--- Scripts/World/LoadRegions.cs
+++ Scripts/World/LoadRegions.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadChunks2 : MonoBehaviour
+public class LoadRegions : MonoBehaviour
 {
     [SerializeField]
     private World world;
+
+    List<RegionPos> region = new List<RegionPos>();
 
     // Start is called before the first frame update
     void Start()
